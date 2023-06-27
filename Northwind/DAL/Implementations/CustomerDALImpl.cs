7d1b14a3a59c62a86049ded99942cb8ac41cfd0b
DanielPPR <dvargas30529@ufide.ac.cr>
2023-06-27 16:47:07 -0600

    Generic Y GetAll

---
+++ Northwind/DAL/Implementations/CustomerDALImpl.cs
@@ -0,0 +1,70 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class CustomerDALImpl : ICustomerDAL
+    {
+        private NorthwindContext _northwindContext;
+        private UnidadDeTrabajo<Customer> unidad;
+
+        public bool Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<Customer> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> Find(Expression<Func<Customer, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            IEnumerable<Customer> customers = null;
+            using (unidad = new UnidadDeTrabajo<Customer>(new NorthwindContext()))
+            {
+                customers = unidad.genericDAL.GetAll();
+
+
+            }
+
+            return customers;
+        }
+
+        public bool Remove(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<Customer> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer SingleOrDefault(Expression<Func<Customer, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Northwind/DAL/Implementations/UnidadDeTrabajo.cs
@@ -0,0 +1,46 @@
+﻿using DAL.Implementations;
+using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Implementations
+{
+    public class UnidadDeTrabajo<T> : IDisposable where T : class
+    {
+        private readonly NorthwindContext context;
+        //public IDALGenerico<Queja> quejaDAL;
+        //public IDALGenerico<TablaGeneral> tablaDAL;
+        public IDALGenerico<T> genericDAL;
+
+
+        public UnidadDeTrabajo(NorthwindContext _context)
+        {
+            context = _context;
+            genericDAL = new DALGenericoImpl<T>(context);
+          
+        }
+
+        public bool Complete()
+        {
+            try
+            {
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                string msj = e.Message;
+                return false;
+            }
+
+        }
+
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
